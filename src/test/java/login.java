45ef8a2c026b97d8e6da396dcfabce778f6ccfdd
Marwa Ahmed <marwadawa993@gmail.com>
2023-03-19 12:53:13 +0200

    initial commit

---
+++ src/test/java/login.java
@@ -0,0 +1,42 @@
+import io.goodforgod.http.common.HttpStatus;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import org.junit.*;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class login {
+    @BeforeClass
+    public static void createRequestSpecification() {
+
+        requestSpecification = new RequestSpecBuilder().
+                setBaseUri("https://pay2.foodics.dev/cp_internal/").
+                build();
+    }
+    @Test
+    public void login_checkStatusCode_expectHttp200() {
+
+        given().auth()
+                .basic("merchant@foodics.com", "123456").spec(requestSpecification)
+                .when()
+                .get("login")
+                .then().assertThat().statusCode(200);
+    }
+    @Test
+    public void whoami_checkStatusCode_expectHttp200() {
+
+        given().auth()
+                .basic("merchant@foodics.com", "123456").spec(requestSpecification)
+                .when()
+                .get("whoami")
+                .then().assertThat().statusCode(200);
+    }
+}
+
+
+
